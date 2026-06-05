5b659d9b3438740c44d2bc5de1a8f7b14ed00313
satish <vallem.pspk@gmail.com>
2026-06-05 13:39:29 +0530

    integrating datadog with aws as terraform template

---
+++ Datadog_aws/variables.tf
@@ -0,0 +1,17 @@
+variable "api_key" {
+    type = string
+    default = "16810d4ecd76f6aaa1e57fed48132957"
+  
+}
+
+variable "app_key" {
+    type = string
+    default = "ddapp_M5v0KaU4Uv4nvVo2HXa0HOFoc5fH2ODAaB"
+  
+}
+
+variable "dd_site" {
+    type = string
+    default = "us5.datadoghq.com"
+  
+}
